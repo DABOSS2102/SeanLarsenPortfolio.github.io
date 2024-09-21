cec0c592bcf454917123b3fa3cbd7fb99b9ff72c
Sean Larsen <63306306+DABOSS2102@users.noreply.github.com>
2024-09-21 23:19:43 +0000

    Updated Items

--- src/BlazorApp/Models/Project.cs
+++ src/BlazorApp/Models/Project.cs
@@ -2,6 +2,7 @@ namespace BlazorApp.Models;
 
 public class Project
 {
+    public string Stage { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Url { get; set; } = string.Empty;
